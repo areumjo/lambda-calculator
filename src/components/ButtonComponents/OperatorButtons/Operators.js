5823b3791c3434acc699682077dc9c6051b65edf
Areum Jo <areumjo1@gmail.com>
2019-07-16 15:45:04 -0400

    Imported operator data and passes it to operatorBtn

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
+import OperatorButton from "./OperatorButton.js";
 //import any components needed
+import { operators } from "../../../data.js";
 
 //Import your array data to from the provided data file
-
+console.log(operators);
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [ope] = useState(operators);
+  console.log(operators);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {ope.map( e => <OperatorButton operator={e.char} />)}
     </div>
   );
 };
+
+export default Operators;
